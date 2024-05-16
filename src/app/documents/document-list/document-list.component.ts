fe28ae24df5a1550b146ef05e7328d0ae3aa9c02
annmarieinlr <wor20005@byui.edu>
2024-05-16 16:28:52 -0500

    implemented documentListComponent

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document(
+      'Doc1',
+      'name1',
+      'description1',
+      'url1'
+    ), 
+    new Document(
+      'Doc2',
+      'name2',
+      'description2',
+      'url2'
+    ), 
+    new Document(
+      'Doc3',
+      'name3',
+      'description3',
+      'url3'
+    ), 
+    new Document(
+      'Doc4',
+      'name4',
+      'description4',
+      'url4'
+    ), 
 
+  ]
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
