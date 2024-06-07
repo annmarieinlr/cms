6dcd1b66c1426796f0a57932a3a497a38d50b09d
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-06-06 23:18:57 -0500

    added the addDocument function

--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -8,10 +8,28 @@ import { Document } from './document.model';
 })
 export class DocumentService {
   documentListChangedEvent = new Subject<Document[]>();
+  maxDocumentId: number;
 
   documents: Document[] = MOCKDOCUMENTS;
 
-  constructor() { }
+  constructor() { 
+    this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
+  }
+
+  addDocument(newDocument: Document) {
+    if (!newDocument) {
+      return;
+    }
+
+    this.maxDocumentId++;
+    newDocument.id = this.maxDocumentId.toString();
+    this.documents.push(newDocument);
+
+    // Notify subscribers about changes to the documents list
+    const documentsListClone = this.documents.slice();
+    this.documentListChangedEvent.next(documentsListClone);
+  }
 
   getDocuments(): Document[] {
     return this.documents.slice();
@@ -26,9 +44,23 @@ export class DocumentService {
     const pos = this.documents.indexOf(document);
     if (pos < 0) return;
     this.documents.splice(pos, 1);
-    this.documentChangedEvent.emit(this.documents.slice());
+    this.documentListChangedEvent.next(this.documents.slice());
   }
 
-  documentSelectedEvent = new EventEmitter<Document>();
-  documentChangedEvent = new EventEmitter<Document[]>();
+  getMaxId(): number {
+    let maxId = 0;
+
+    for (const document of this.documents) {
+      const currentId = parseInt(document.id, 10); // Convert document id to number
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+
+    return maxId;
+  }
 }
+
+//   documentSelectedEvent = new EventEmitter<Document>();
+//   documentChangedEvent = new EventEmitter<Document[]>();
+// }
