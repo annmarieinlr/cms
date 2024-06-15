32b9e400095b136f89dc4ea318885cb03a166bff
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-06-15 17:03:56 -0500

    form validation for new contacts

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
-export class ContactEditComponent {
-groupContacts: any;
+export class ContactEditComponent  implements OnInit {
+  @ViewChild('f') contactForm: NgForm;
+  groupContacts: any;
   onCancel() {
     console.log("cancel");
+  }
+
+  onSubmit(form: NgForm) {
+
+  }
+
+  ngOnInit(): void {
+    
   }
 }
