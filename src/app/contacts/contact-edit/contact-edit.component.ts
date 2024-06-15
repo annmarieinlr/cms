0dfc35eb4f98cb41660f44fe8cb33c29de5e4320
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-06-15 17:27:43 -0500

    updated contact edit

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
@@ -8,16 +11,55 @@ import { NgForm } from '@angular/forms';
 })
 export class ContactEditComponent  implements OnInit {
   @ViewChild('f') contactForm: NgForm;
-  groupContacts: any;
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
+
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+
   onCancel() {
-    console.log("cancel");
+    this.router.navigate(['/contacts'], { relativeTo: this.route });
   }
 
   onSubmit(form: NgForm) {
+    const value = form.value;
+    const newContact = new Contact(null, value.name, value.email, value.phone, value.imageUrl, this.groupContacts)
 
+    if (this.editMode) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    }
+    else {
+      this.contactService.addContact(newContact);
+    }
+    this.router.navigate(['/documents'], { relativeTo: this.route });
   }
+  
 
   ngOnInit(): void {
-    
-  }
+    this.route.params.subscribe(params => {
+    this.id = params['id'];
+
+    if (this.id === undefined || this.id === null) {
+     this.editMode = false;
+     return;
+    }
+
+    this.originalContact = this.contactService.getContact(this.id);
+
+    if (this.originalContact === undefined || this.originalContact === null) {
+      return;
+    }
+
+    this.editMode = true;
+
+    this.contact = JSON.parse(JSON.stringify(this.originalContact));
+  });
+}
 }
