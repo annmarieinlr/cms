9272e68b54aa92731a28fd89ecf0385b49b5021a
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-06-22 19:27:24 -0500

    messages changed

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Message } from '../message.model';
 import { MessageService } from '../message.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cms-message-list',
@@ -9,16 +10,32 @@ import { MessageService } from '../message.service';
 })
 export class MessageListComponent implements OnInit {
   messages: Message[] = [];
+  messagesSubscription: Subscription;
 
   constructor(private messageService: MessageService) {}
   ngOnInit() {
-    this.messages = this.messageService.getMessages(); // Initialize messages array
-    this.messageService.messageChangedEvent.subscribe(messages => {
-      this.messages = messages; // Update messages array when it changes
-    })
+    // this.messages = this.messageService.getMessages(); // Initialize messages array
+    // this.messageService.messageChangedEvent.subscribe(messages => {
+    //   this.messages = messages; // Update messages array when it changes
+    // })
+
+    this.messagesSubscription = this.messageService.getMessages()
+    .subscribe(
+      (messages: Message[]) => {
+        this.messages = messages;
+      },
+      (error) => {
+        console.error('Error fetching contacts:', error);
+        // Handle error as needed
+      }
+    );
+    this.messagesSubscription = this.messageService.messageChangedEvent.subscribe((contactsList: Message[]) => {
+      
+    });
   }
-  onMessageSelected(message: Message) {
-    // Implement your logic when a message is selected
-    console.log('Message selected:', message);
   }
-}
+  // onMessageSelected(message: Message) {
+  //   // Implement your logic when a message is selected
+  //   console.log('Message selected:', message);
+  // }
+
--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,6 +1,9 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +11,67 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
 export class MessageService {
 
   messages: Message[] = [];
+  maxMessageId: number;
+  private messagesUrl = 'https://cmsproject-64c63-default-rtdb.firebaseio.com/messages.json';
 
-
-  constructor() {
+  constructor( private http: HttpClient) {
     this.messages = MOCKMESSAGES;
    }
+
+
 
   addMessage(message: Message){
     this.messages.push(message);
     this.messageChangedEvent.emit(this.messages.slice());
+    this.storeMessages();
   }
   messageChangedEvent = new EventEmitter<Message[]>();
+    
 
-  getMessages(): Message[] {
-    return this.messages.slice();
+  // getMessages(): Message[] {
+  //   return this.messages.slice();
+    
+  // }
+
+  getMessages(): Observable<Message[]> {
+    return this.http.get<Message[]>('https://cmsproject-64c63-default-rtdb.firebaseio.com/messages.json')
+      .pipe(
+        tap((messages: Message[]) => {
+          this.messages = messages;
+          this.maxMessageId = this.getMaxId();
+          //this.messages.sort((a, b) => a.name.localeCompare(b.name)); // Sort by name
+          const messageListClone = this.messages.slice();
+          this.messageChangedEvent.next(messageListClone);
+        }),
+        catchError((error) => {
+          console.error('Error fetching contacts:', error);
+          return throwError(error);
+        })
+      );
+  }
+
+  storeMessages() {
+     // Convert the contacts array into a string format
+     const contactsString = JSON.stringify(this.messages);
+
+     // PUT request to store the contacts in the Firebase database
+     this.http.put(this.messagesUrl, contactsString, {
+       headers: new HttpHeaders({
+         'Content-Type': 'application/json',
+       }),
+     })
+       .subscribe((response: Response) => {
+         console.log(response);
+       });
+  }
+  getMaxId(): number {
+    let maxId = 0;
+    for (let message of this.messages) {
+      let currentId = parseInt(message.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
   }
 }
