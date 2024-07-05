1f17300e1f215633dd226d27808f471227166c97
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-07-05 17:47:57 -0500

    starting models

--- server/routes/app2.js
+++ server/routes/app2.js
@@ -1,27 +1,14 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// //From video:
-
-// // app.router.get('/', function(req, res, next) { 
-// //   res.sendFile(path.join(__dirname, 'dist/cms/browser/index.html'));
-// // });
-
-
-// // Set the view engine to EJS
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-// // Define routes here
-// app.get('/', (req, res) => { res.render('index', { title: 'CMS' }); });
-// module.exports = app;
-
 var express = require('express');
 var router = express.Router();
+
+const Message = require('../models/messages');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, 'dist/cms/browser/index.html'));
 });
 
+router.get('/messages', function(req, res, next) {
+  
+});
 module.exports = router;
